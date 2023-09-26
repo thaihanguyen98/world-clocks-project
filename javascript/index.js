23d054041620a79e961b6fe3c815472f97384396
thaihanguyen98 <142948159+thaihanguyen98@users.noreply.github.com>
2023-09-26 12:19:32 +1000

    Updated CSS

--- javascript/index.js
+++ javascript/index.js
@@ -22,18 +22,23 @@ function updateTime() {
   );
 }
 
-updateTime();
-setInterval(updateTime, 1000);
+let intervalID = 0;
 
 function updateCity(event) {
-  let cityTimeZone = event.target.value;
-  if (cityTimeZone === "current") {
-    cityTimeZone = moment.tz.guess();
-  }
-  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
-  let cityTime = moment().tz(cityTimeZone);
-  let citiesElement = document.querySelector("#cities");
-  citiesElement.innerHTML = `
+  intervalID = setInterval(() => {
+    let cityTimeZone = event.target.value;
+    if (cityTimeZone === "current") {
+      cityTimeZone = moment.tz.guess();
+    }
+    let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+    let cityTime = moment().tz(cityTimeZone);
+    let citiesElement = document.querySelector("#cities");
+    let backElement = document.querySelector("#back-button");
+
+    backElement.innerHTML = `<div class ="backToOverview">
+    <a href="/" class = "backButton">< Back to overview</a></div>`;
+
+    citiesElement.innerHTML = `
     <div class="city">
           <div>
             <h2>${cityName}</h2>
@@ -43,9 +48,12 @@ function updateCity(event) {
             "h:mm:ss"
           )} <small>${cityTime.format("A")}</small>
         </div>
-        </div>
-`;
+        </div>`;
+  }, 1000);
 }
+
+updateTime();
+setInterval(updateTime, 1000);
 
 let citiesSelectElement = document.querySelector("#city");
 citiesSelectElement.addEventListener("change", updateCity);
